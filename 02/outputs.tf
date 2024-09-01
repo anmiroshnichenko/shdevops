db041f146d5dc0b7308f38f0e76d7df5f8125fdd
Aleksandr Miroshnichenko <alexand.miroshnichenko@yandex.ru>
2024-09-01 19:47:07 +0300

    Changed hw-02.md

--- 02/outputs.tf
+++ 02/outputs.tf
@@ -1,7 +1,15 @@
-output "external_vm" {
+output "external_platform-web" {
 value = yandex_compute_instance.platform.network_interface[0].nat_ip_address
 }
 
-output "internal_vm" {
+output "internal_platform-web" {
 value = yandex_compute_instance.platform.network_interface[0].ip_address
+}
+
+output "external_platform-db" {
+value = yandex_compute_instance.platform-db.network_interface[0].nat_ip_address
+}
+
+output "internal_platform-db" {
+value = yandex_compute_instance.platform-db.network_interface[0].ip_address
 }
--- 02/variables.tf
+++ 02/variables.tf
@@ -1,4 +1,5 @@
-###cloud vars
+### cloud vars
+
 # variable "token" {
 #   type        = string
 #   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
@@ -21,9 +22,23 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+
+variable "zone" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+### Network vars
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "cidr" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
@@ -33,11 +48,20 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
-
-###ssh vars
-
+variable "vpc_1_name" {
+  type        = string
+  default     = "develop_1"
+  description = "Subnet name"
+}
+### ssh vars
 variable "vms_ssh_root_key" {
   type        = string
   default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpEVTvLfMVp0ZydLogg5s1D8WzKQaY2n0MqDsBNX2YkoaHP1vHs0Aqg5ICFaKXwVnc1ns6ntMO8hYYm8ShO6fw47MFYm6JtnK7YItJT+MAj8gPYId1HiEaE47e1NSF6a5IVglw6eTo+KLHEhl+YS+zew/UcNcOYLcBP3hWdtGQmjRygy6o9BXkwUlDQ6PgwZ4A+8gd3wEkHNlabqB/43rJsDzJzMGRcduEdQAZh4A6KwXs3S9BpidSaAwwoV2DyK95PZl2w6/UfvdHeSjUkzoe+EiMbiDGXGkekDsuHKpm6Npdok3rd3wW8YgYHYimuT1VuUYw2aONffA45xIN3/udpT58JAfV1F7NrAq4g3nu5eFR97bPM9xUXYeW7Wy4x8VyzKLWKZH4LjyWiWp9bGsaJXiEsY/tIbYNpA7ZVHZszk8ObPLODZ1IVfAUNKHNMtZ9vrrWp0anRIeq2zfOCvM8bGfKm0gUJ9j6TWslEmE7uK6uSQLv7lqts7Aj2hq3rG0= miroshnichenko@pc5"
   description = "ssh-keygen -t ed25519"
+}
+### image vars
+variable "vm_web_image_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "OS image for virtual machine"
 }
