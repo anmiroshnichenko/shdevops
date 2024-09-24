8f7962c8f425d612da0273caf566a63f6bd5b7bc
Aleksandr Miroshnichenko <alexand.miroshnichenko@yandex.ru>
2024-09-24 21:12:21 +0300

    Added new file  vpc/README.MD

--- 03/outputs.tf
+++ 03/outputs.tf
@@ -25,13 +25,24 @@
 #     ]  
 # } 
 
+# output "servers" {
+#     value = [for instance in [yandex_compute_instance.db, yandex_compute_instance.web] : [for k in instance :{
+#         "name" = k.name, 
+#         "id"    = k.id, 
+#         "fqdn"  = k.fqdn} 
+#         ]
+#     ]    
+# } 
+
 output "servers" {
-    value = [for instace in [yandex_compute_instance.db, yandex_compute_instance.web] : [for k in instace :{
-        "name" = k.name, 
-        "id"    = k.id, 
-        "fqdn"  = k.fqdn} 
-        ]
-    ]    
+    value = toset([for instance in [yandex_compute_instance.db, yandex_compute_instance.web] : [for k in instance : {"name" = k.name}
+
+    # {
+                # "name" = k.name, 
+        # "id"    = k.id, 
+        # "fqdn"  = k.fqdn } 
+    ]
+    ]  )  
 } 
 
 
