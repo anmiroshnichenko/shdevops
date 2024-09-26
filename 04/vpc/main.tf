4176a948902bd1a357ae81a71ab6c4d2e6b499fc
Aleksandr Miroshnichenko <alexand.miroshnichenko@yandex.ru>
2024-09-26 19:25:20 +0300

    modified vpc/variables.tf

--- 04/vpc/main.tf
+++ 04/vpc/main.tf
@@ -15,16 +15,35 @@ resource "yandex_vpc_network" "develop" {
 }
 
 #создаем подсеть
-resource "yandex_vpc_subnet" "develop" {
-  name           = "${var.vpc_name}-${var.default_zone}"
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
-}
+# resource "yandex_vpc_subnet" "develop" {
+#   name           = "${var.vpc_name}-${var.default_zone}"
+#   zone           = var.default_zone
+#   network_id     = yandex_vpc_network.develop.id
+#   v4_cidr_blocks = var.default_cidr
+# }
 
+# resource "yandex_vpc_subnet" "develop"  {
+#     for_each = var.subnets 
+#     # v4_cidr_blocks = each.value
+#     # vpc_id     = aws_vpc.vpc.id
+# }
 # resource "yandex_vpc_subnet" "develop_b" {
 #   name           = "${var.vpc_name}-${var.zone}"
 #   zone           = var.zone
 #   network_id     = yandex_vpc_network.develop.id
 #   v4_cidr_blocks = var.cidr
+# }
+# resource "aws_security_group" "env0_security_group" {
+#   name        = "env0-security-group"
+#   vpc_id      = aws_vpc.env0_vpc.id
+
+#   dynamic "ingress" {
+#     for_each = [for rule in var.security_group_rules : rule if rule.type == "ingress"]
+#     content {
+#       from_port   = ingress.value.from_port
+#       to_port     = ingress.value.to_port
+#       protocol    = ingress.value.protocol
+#       cidr_blocks = ingress.value.cidr_blocks
+#     }
+#   }  
 # }
--- 04/vpc/variables.tf
+++ 04/vpc/variables.tf
@@ -33,3 +33,13 @@ variable "cidr" {
   default     = ["10.0.2.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
+
+variable "subnets" {
+  description = "A list of maps, where each map contains subnet-specific attributes"
+  type = list(object({ zone = string, cidr = string }))
+  default = [
+    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
+    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
+    { zone = "ru-central1-c", cidr = "10.0.3.0/24" },
+  ]
+}
